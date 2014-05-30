77068c138cf2749dfddd6090cf7bb163843c1b3b
EricSchles <ericschles@gmail.com>
2014-05-30 05:21:55 -0700

    alice added

---
+++ chapter1/typing/alice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ 
+  cout << "Was that Alice?" << endl;
+  cout << "I'm afraid I'm" << endl;
+  cout << "rather late!" << endl;
+
+  cin.ignore();
+  return 0;
+}
