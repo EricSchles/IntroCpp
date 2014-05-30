2ab650bba015908be59d1816cf1a889a234fcabc
EricSchles <ericschles@gmail.com>
2014-05-30 06:22:43 -0700

    loops added

---
+++ chapter1/typing/loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n = 1;
+  while (n < 6) {
+    cout << n << " ";
+    n = n + 1;
+  }
+
+  n = 0;
+  while (n < 6 ) {
+    cout << n << "\n";
+    n++;
+  }
+
+  return 0;
+}
