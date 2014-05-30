2b8a245078b2bacb4f79081c05d10a2e4aeac15a
EricSchles <ericschles@gmail.com>
2014-05-30 06:20:04 -0700

    things updated

---
+++ chapter1/typing/flow.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double x = 0.0;
+  double y = 0.0;
+
+  cout << "Enter value of x: ";
+  cin >> x;
+  cout << "Enter value of y: ";
+  cin >> y;
+  if (x > y) {
+    cout << "The max is " << x << endl;
+  } else {
+    cout << "The max is " << y << endl;
+  }
+  cout << "Press ENTER to exit."<<endl;
+  cin.ignore();
+  cin.ignore();
+  return 0;
+}
