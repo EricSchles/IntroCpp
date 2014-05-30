dd39419dae8118c3102172de7c7cd886214129ac
EricSchles <ericschles@gmail.com>
2014-05-30 05:32:21 -0700

    grabbing 2 added

---
+++ chapter1/typing/grabbing2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  double x =0.0;
+  cout << "Enter value of x: ";
+  cin >> x;
+  cout << "Twice that number is: ";
+  cout << x * 2.0;
+  cout << endl;
+  cout << "Press ENTER to exit.";
+  cin.ignore();
+  cin.ignore();
+  return 0;
+}
