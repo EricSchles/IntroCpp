ecf91be450219fc838dcb82dafed2100667c84fb
EricSchles <ericschles@gmail.com>
2014-05-31 06:50:59 -0700

    cra cra

--- chapter2/typing/first.cpp
+++ chapter2/typing/first.cpp
@@ -9,5 +9,8 @@ int main() {
   cout << static_cast<char>(c);
   cout << my_name;
   cout << "Testing this " " out";
+  char very_long[] =  "Apparrently you can do this"
+    "because c++ is crazy"
+    "so crazy infact";
   return 0;
 }
